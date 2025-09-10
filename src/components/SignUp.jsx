d54aa9595489fcbb71a13f3725868bb806b6a93c
hayleamlak <hayleamlaktesfaye12@gmail.com>
2025-09-10 20:43:38 +0300

    add the sign up page color

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -19,7 +19,7 @@ const SignUp = () => {
 
   return (
     <form onSubmit={handleSignUp}>
-      <h2>sign up</h2>
+      <h2>Sign up</h2>
       <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required />
       <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
       <button type="submit">Sign Up</button>
