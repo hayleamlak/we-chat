8ce8116982a251743493a78ff541d59ee8a03eb2
hayleamlak <hayleamlaktesfaye12@gmail.com>
2025-10-13 19:30:07 +0300

    singup

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -5,12 +5,12 @@ import { auth } from "../firebase/firebase";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [password, setPassword] = usestate("");
 
   const handleSignUp = async (e) => {
     e.preventDefault();
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
+      await createUserWithEmailAndPassword(auth,email, password);
       alert("Account created successfully!");
     } catch (error) {
       alert("Error signing up: " + error.message);
@@ -19,7 +19,7 @@ const SignUp = () => {
 
   return (
     <form onSubmit={handleSignUp}>
-      <h2>Singin page</h2>
+      <h2>Singn in </h2>
       
       <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required />
       <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
