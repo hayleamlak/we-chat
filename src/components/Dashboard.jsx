dd9c406c7e6c49af6ff98d42b490f20b289e8418
hayleamlak <hayleamlaktesfaye12@gmail.com>
2025-10-14 15:24:03 +0300

    creat on logout function

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,7 +5,7 @@ export default function Dashboard({ user, onLogout }) {
   return (
     <div style={{ maxWidth: 300, margin: "auto", textAlign: "center" }}>
       <h2>Welcome, {user.email}</h2>
-      <button onClick={onLogout} style={{ padding: 10, marginTop: 20 }}>
+      <button onClick={onlogout} style={{ padding: 10, marginTop: 20 }}>
  
       </button>
     </div>
